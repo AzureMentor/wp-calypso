ea70a4f632f5c1959e7fe32f73a586f2ad62496a
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2019-06-26 14:08:02 +0530

    1. Add a default destination if the value from cookie is missing for some reason 2. Rename isElgibleForCheckoutToChecklist to isEligibleForSignupDestination because the destination is no longer just Checklist, so that name does not make sense.

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -69,7 +69,7 @@ import {
 import getContactDetailsCache from 'state/selectors/get-contact-details-cache';
 import getUpgradePlanSlugFromPath from 'state/selectors/get-upgrade-plan-slug-from-path';
 import isDomainOnlySite from 'state/selectors/is-domain-only-site';
-import isEligibleForCheckoutToChecklist from 'state/selectors/is-eligible-for-checkout-to-checklist';
+import isEligibleForSignupDestination from 'state/selectors/is-eligible-for-checkout-to-checklist';
 import { getStoredCards } from 'state/stored-cards/selectors';
 import { isValidFeatureKey } from 'lib/plans/features-list';
 import { getPlan, findPlansKeys } from 'lib/plans';
@@ -388,6 +388,10 @@ export class Checkout extends React.Component {
 		} else {
 			receiptId = this.props.purchaseId ? this.props.purchaseId : ':receiptId';
 		}
+		const destinationFromStore = retrieveSignupDestination();
+		const signUpdestination = destinationFromStore
+			? destinationFromStore.replace( ':receiptId', receiptId )
+			: `/view/${ selectedSiteSlug }`;
 
 		if ( hasRenewalItem( cart ) ) {
 			renewalItem = getRenewalItems( cart )[ 0 ];
@@ -402,8 +406,7 @@ export class Checkout extends React.Component {
 		}
 
 		if ( cart.create_new_blog ) {
-			const destination = retrieveSignupDestination();
-			return `${ destination }/${ receiptId }`;
+			return `${ signUpdestination }/${ receiptId }`;
 		}
 
 		if ( ! selectedSiteSlug ) {
@@ -428,9 +431,7 @@ export class Checkout extends React.Component {
 					plan: 'paid',
 				} );
 
-				const destination = retrieveSignupDestination();
-
-				return `/${ destination }?d=gsuite`;
+				return `/${ signUpdestination }?d=gsuite`;
 			}
 
 			// Maybe show either the G Suite or Concierge Session upsell pages
@@ -470,10 +471,8 @@ export class Checkout extends React.Component {
 
 		const queryParam = displayModeParam ? `?${ displayModeParam }` : '';
 
-		if ( this.props.isEligibleForCheckoutToChecklist & ( ':receiptId' !== receiptId ) ) {
-			const destination = retrieveSignupDestination();
-
-			return `${ destination }${ queryParam }`;
+		if ( this.props.isEligibleForSignupDestination & ( ':receiptId' !== receiptId ) ) {
+			return `${ signUpdestination }${ queryParam }`;
 		}
 
 		if ( this.props.isJetpackNotAtomic ) {
@@ -801,7 +800,7 @@ export default connect(
 			isNewlyCreatedSite: isNewSite( state, selectedSiteId ),
 			contactDetails: getContactDetailsCache( state ),
 			userCountryCode: getCurrentUserCountryCode( state ),
-			isEligibleForCheckoutToChecklist: isEligibleForCheckoutToChecklist(
+			isEligibleForSignupDestination: isEligibleForSignupDestination(
 				state,
 				selectedSiteId,
 				props.cart
